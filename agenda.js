c8697c5e13c8344669c5ef685f9a6da2a8fdbc80
samnijens <Sam.nijens@gmail.com>
2026-07-16 21:09:13 +0200

    Integrate Firestore for event handling in agenda.js
    Refactor agenda.js to include Firestore functionality for event management.

--- agenda.js
+++ agenda.js
@@ -1,28 +1,53 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 
 import {
 
-    onAuthStateChanged,
+onAuthStateChanged,
 
-    signOut
+signOut
 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+}
 
+from
 
-
-
-
-onAuthStateChanged(auth, (user) => {
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 
-    if (!user) {
+import {
+
+collection,
+
+getDocs,
+
+addDoc,
+
+deleteDoc,
+
+doc
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 
-        window.location.href = "index.html";
 
 
-    }
+onAuthStateChanged(auth,(user)=>{
+
+
+if(!user){
+
+window.location.href="index.html";
+
+return;
+
+}
+
+
+startCalendar();
 
 
 });
@@ -31,18 +56,159 @@ onAuthStateChanged(auth, (user) => {
 
 
 
-const logoutButton =
-document.getElementById("logoutButton");
+async function startCalendar(){
+
+
+const calendarEl =
+document.getElementById("calendar");
 
 
 
-logoutButton.addEventListener("click", async () => {
+let events=[];
 
 
-    await signOut(auth);
+
+const snapshot =
+await getDocs(
+collection(db,"events")
+);
 
 
-    window.location.href = "index.html";
+
+snapshot.forEach((item)=>{
+
+
+events.push({
+
+id:item.id,
+
+title:item.data().title,
+
+start:item.data().start
+
+});
 
 
 });
+
+
+
+
+
+const calendar =
+new FullCalendar.Calendar(calendarEl,{
+
+
+initialView:"dayGridMonth",
+
+
+locale:"nl",
+
+
+firstDay:1,
+
+
+headerToolbar:{
+
+
+left:"prev,next today",
+
+center:"title",
+
+right:
+"dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+
+
+},
+
+
+events:events,
+
+
+
+dateClick:async(info)=>{
+
+
+const title =
+prompt("Nieuwe afspraak:");
+
+
+
+if(title){
+
+
+await addDoc(
+collection(db,"events"),
+{
+
+title:title,
+
+start:info.dateStr
+
+}
+
+);
+
+
+location.reload();
+
+
+}
+
+
+},
+
+
+
+eventClick:async(info)=>{
+
+
+if(confirm("Afspraak verwijderen?")){
+
+
+await deleteDoc(
+
+doc(
+db,
+"events",
+info.event.id
+
+)
+
+);
+
+
+location.reload();
+
+
+}
+
+
+}
+
+
+
+});
+
+
+calendar.render();
+
+
+}
+
+
+
+
+
+document
+.getElementById("logoutButton")
+.onclick=()=>{
+
+
+signOut(auth);
+
+
+window.location.href="index.html";
+
+
+};
