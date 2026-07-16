db0224cbeee8fce9dc1d4d323c6f35c4393e11b4
samnijens <Sam.nijens@gmail.com>
2026-07-16 21:13:46 +0200

    Update Firebase imports and configuration

--- firebase.js
+++ firebase.js
@@ -1,26 +1,39 @@
-import { initializeApp } from "firebase/app";
-import { 
-  getAuth 
-} from "firebase/auth";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-import {
-  getFirestore
-} from "firebase/firestore";
+
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyARKx42cvUgW9Qpf33DmBirfMdSERi8Jl8",
+
   authDomain: "snejin-agenda-7db33.firebaseapp.com",
+
   projectId: "snejin-agenda-7db33",
+
   storageBucket: "snejin-agenda-7db33.firebasestorage.app",
+
   messagingSenderId: "494528590764",
-  appId: "1:494528590764:web:c177e87c3842fc9c55c009"
+
+  appId: "1:494528590764:web:2a92bff6dae6515055c009"
+
 };
+
 
 
 const app = initializeApp(firebaseConfig);
 
 
+
 export const auth = getAuth(app);
+
 
 export const db = getFirestore(app);
